8bd869adbd225db68e6d931c03c2908e5e94c9d2
jibrinmasud <jibrinmasud494@gmail.com>
2024-09-21 19:14:36 +0100

    Complete ProductAPI Microservice

--- ProductApi.Infrastucture/Data/ProductDbContext.cs
+++ ProductApi.Infrastucture/Data/ProductDbContext.cs
@@ -3,11 +3,10 @@ using ProductApi.Domain.Entities;
 
 namespace ProductApi.Infrastucture.Data
 {
-    public class ProductDbContext:DbContext
+    public class ProductDbContext: DbContext
     {
         public ProductDbContext(DbContextOptions<ProductDbContext> options):base(options)
-        {
-            
+        {    
         }
         public DbSet<Product> Products { get; set; }
     }
